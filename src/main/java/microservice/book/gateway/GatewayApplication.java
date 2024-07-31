c009b2665ece88e4d4a18e827b45ff1bd8edd520
shinjaewon99 <tlswodnjs99@naver.com>
2024-07-31 22:45:27 +0900

    feat: 리본 로드밸런싱 Ping 설정 추가

--- src/main/java/microservice/book/gateway/GatewayApplication.java
+++ src/main/java/microservice/book/gateway/GatewayApplication.java
@@ -1,9 +1,12 @@
 package microservice.book.gateway;
 
+import microservice.book.gateway.configuration.RibbonConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.ribbon.RibbonClients;
 
 @SpringBootApplication
+@RibbonClients(defaultConfiguration = RibbonConfiguration.class)
 public class GatewayApplication {
 
 	public static void main(String[] args) {
